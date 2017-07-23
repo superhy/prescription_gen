10230f2f58e8e1bbf3ef0e17d29e2817446daa35
superhy <superhy@DESKTOP-AMM4OG7>
2017-07-23 23:27:59 +0800

    load_matrix(2017.7.23)

--- layer/text_gen.py
+++ layer/text_gen.py
@@ -5,9 +5,28 @@ Created on 2017年7月20日
 
 @author: superhy
 '''
+from interface import word_embedding
+import numpy as np
 
-def loadMatrixfromData(seg_patient_path, zhiliao_path, nb_yao):
-    pass
+def loadMatrixfromData(patient_setences, wordvec_model, yaofangs, patient_cnt_len, nb_yao):
+    
+    vocab = wordvec_model.vocab.key()
+    
+    x = np.zeros((len(patient_setences), patient_cnt_len, wordvec_model.vector_size), dtype=np.float32)
+    y = np.zeros((len(patient_setences), nb_yao), dtype=np.bool)
+    
+    for i, sentence in enumerate(patient_setences):
+        for j, word, in enumerate(sentence):
+            if word in vocab:
+                vocab_vector = word_embedding.getWordVec(wordvec_model, word)
+            else:
+                vocab_vector = np.zeros((wordvec_model.vector_size), dtype=np.float32)
+            # use Chinese wordvec as the training space
+            x[i, j] = vocab_vector
+        for yao_id in yaofangs[i]:
+            y[i, yao_id] = 1
+            
+    return x, y
 
 if __name__ == '__main__':
     pass
